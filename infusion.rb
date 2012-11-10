8d8b7bc9b285a203ae68c9f5defcea89a375a199
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-11-11 01:21:49 +1300

    libogg-1.3.0

---
+++ infusion.rb
@@ -0,0 +1,26 @@
+
+#
+#  This file is part of the "Teapot" project, and is released under the MIT license.
+#
+
+required_version "0.1"
+
+define_package "libogg-1.3.0" do |package|
+	package.build(:all) do |platform, environment|
+		environment.use in: package.source_path do |config|
+			Commands.run("make", "clean") if File.exist? "Makefile"
+				
+			Commands.run("./configure",
+				"--prefix=#{platform.prefix}",
+				"--disable-dependency-tracking",
+				"--enable-shared=no",
+				"--enable-static=yes",
+				*config.configure
+			)
+			
+			Commands.run("make install")
+		end
+		
+		Commands.run("cp", "FindOgg.cmake", platform.cmake_modules_path)
+	end
+end

2ad7e7c02fbba1480f175f5e889304a87deb28d7
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-12-07 14:38:17 +1300

    Update to new dependency based build.

--- infusion.rb
+++ infusion.rb
@@ -3,22 +3,28 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.1"
+required_version "0.5"
 
-define_package "libogg-1.3.0" do |package|
-	package.build(:all) do |platform, environment|
-		environment.use in: package.source_path do |config|
+define_package "ogg" do |package|
+	package.install do |environment|
+		environment.use in:(package.path + 'libogg-1.3.0') do |config|
 			Commands.run("make", "clean") if File.exist? "Makefile"
 				
 			Commands.run("./configure",
-				"--prefix=#{platform.prefix}",
+				"--prefix=#{config.install_prefix}",
 				"--disable-dependency-tracking",
 				"--enable-shared=no",
 				"--enable-static=yes",
 				*config.configure
 			)
 			
-			Commands.run("make install")
+			Commands.run("make", "install")
 		end
+	end
+	
+	package.depends :system
+	
+	package.provides "Library/ogg" do
+		append linkflags "-logg"
 	end
 end

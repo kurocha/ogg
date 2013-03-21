77ef8b8a12fe398ad2b8423b4cb4c50edd0b153c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-03-22 02:48:48 +1300

    Update to support teapot 0.7.0

--- teapot.rb
+++ teapot.rb
@@ -6,8 +6,8 @@
 required_version "0.6"
 
 define_target "ogg" do |target|
-	target.install do |environment|
-		install_external(package.path, "libogg-1.3.0", environment) do |config, fresh|
+	target.build do |environment|
+		build_external(package.path, "libogg-1.3.0", environment) do |config, fresh|
 			if fresh
 				Commands.run("./configure",
 					"--prefix=#{config.install_prefix}",
